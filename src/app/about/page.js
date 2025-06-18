edee379d5e037c3e9e908560c76f8d9a2a904399
shambolic-samreeeed <samreedmaharjan1899@gmail.com>
2025-06-18 08:59:16 +0545

    components

---
+++ src/app/about/page.js
@@ -0,0 +1,10 @@
+import Navbar from "@/components/Navbar";
+
+export default function About(){
+    return (
+        <>
+            <h1>This is about page</h1>
+            
+        </>
+    );
+}
---
+++ src/app/contact-us/page.js
@@ -0,0 +1,9 @@
+import Navbar from "@/components/Navbar";
+
+export default function Contact(){
+    return (
+        <>
+            <h1>contact us page</h1>
+        </>
+    )
+}
